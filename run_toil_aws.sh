2233e5aa28a7834b8fc25d46850c4fb9896950ed
chapmanb <chapmanb@50mail.com>
2017-02-13 13:45:07 -0500

    Toil AWS: fix typos in example command

--- run_toil_aws.sh
+++ run_toil_aws.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 cwltoil --batchSystem=mesos --mesosMaster=mesos-master:5050 \
   --provisioner aws --defaultPreemptable \
-  --preemtableNodeType m4.large:0.1 --maxPreemptableNodes 2 \
+  --preemptableNodeType m4.large:0.1 --maxPreemptableNodes 2 \
   --jobStore=aws:us-east-1:toil-bcbio-jobstore \
-  run_info-cwl-workflow run_info-cwl-workflow/main-run_info-cwl.cwl \
+  run_info-cwl-workflow/main-run_info-cwl.cwl \
   run_info-cwl-workflow/main-run_info-cwl-samples.json
