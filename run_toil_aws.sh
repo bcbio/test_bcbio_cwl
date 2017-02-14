814c46d5ff31b30d345e9865fb16abfdf41fd652
chapmanb <chapmanb@50mail.com>
2017-02-14 05:59:01 -0500

    Toil AWS: extract editable parts of script

--- run_toil_aws.sh
+++ run_toil_aws.sh
@@ -2,9 +2,12 @@
 # Run Toil on AWS with autoscaling
 set -eu -o pipefail
 
-cwltoil --batchSystem=mesos --mesosMaster=mesos-master:5050 \
+LEADER_PRIVATE_IP=10.0.0.XX
+JOB_STORE=aws:us-east-1:toil-bcbio-jobstore
+
+cwltoil --batchSystem=mesos --mesosMaster=$LEADER_PRIVATE_IP:5050 \
   --provisioner aws --defaultPreemptable \
   --preemptableNodeType m4.large:0.1 --maxPreemptableNodes 2 \
-  --jobStore=aws:us-east-1:toil-bcbio-jobstore \
+  --jobStore= $JOB_STORE \
   run_info-cwl-workflow/main-run_info-cwl.cwl \
   run_info-cwl-workflow/main-run_info-cwl-samples.json
