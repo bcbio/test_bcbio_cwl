3820f11f5413d384fbc410be06d527aa50471281
chapmanb <chapmanb@50mail.com>
2017-03-25 14:37:34 -0400

    Restore Toil AWS script after testing

--- run_toil_aws.sh
+++ run_toil_aws.sh
@@ -9,11 +9,11 @@ SPOT_BID=0.1
 MAX_NODES=2
 
 cwltoil \
+  --batchSystem=mesos --mesosMaster=$LEADER_PRIVATE_IP:5050 \
+  --provisioner aws \
+  --defaultPreemptable  --preemptableNodeType $NODE_TYPE:$SPOT_BID --maxPreemptableNodes $MAX_NODES \
+  --nodeType $NODE_TYPE --maxNodes $MAX_NODES \
   --jobStore $JOB_STORE \
   run_info-cwl-workflow/main-run_info-cwl.cwl \
   run_info-cwl-workflow/main-run_info-cwl-samples.json
   
-  #--batchSystem=mesos --mesosMaster=$LEADER_PRIVATE_IP:5050 \
-  #--provisioner aws \
-  #--defaultPreemptable  --preemptableNodeType $NODE_TYPE:$SPOT_BID --maxPreemptableNodes $MAX_NODES \
-  #--nodeType $NODE_TYPE --maxNodes $MAX_NODES \
