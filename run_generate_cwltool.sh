15eada86e64c0aefcef9625b668f15761a9a2695
chapmanb <chapmanb@50mail.com>
2017-04-27 05:35:43 -0400

    Test data: use individual step Docker containers
    Updates to latest CWL compatible pipeline approaches with smaller
    multiple Docker containers.

--- run_generate_cwltool.sh
+++ run_generate_cwltool.sh
@@ -7,3 +7,4 @@ bcbio_vm.py cwl --systemconfig=bcbio_system.yaml run_info-cwl.yaml
 
 CURDIR=`pwd`
 sed -i.bak "s#$CURDIR/testdata#../testdata#" run_info-cwl-workflow/main-run_info-cwl-samples.json
+#cwltool --pack run_info-cwl-workflow/main-run_info-cwl.cwl > run_info-cwl-workflow/main-run_info-cwl-standalone.cwl
