694ded00920ea2438412e4d42ee9cf64c544e605
Roman Valls Guimera <brainstorm@nopcode.org>
2017-07-26 16:50:57 +0200

    Sample commented bash submission script for run_toil.sh on a cluster

--- run_toil.sh
+++ run_toil.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Example batch submission directives for PBS batch system (run as $qsub run_toil.sh).
+#PBS -q normalsp
+#PBS -l ncpus=1
+#PBS -l mem=4G
+#PBS -l walltime=240:00:00
+#PBS -e torque_run_toil_stdout.log
+#PBS -o torque_run_toil_stderr.log
+
 set -eu -o pipefail
 
 #bcbio_vm.py cwlrun toil run_info-cwl-workflow
