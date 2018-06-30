68c413b14cc115e61f014451b8ab4971642b54c4
chapmanb <chapmanb@50mail.com>
2018-06-30 07:36:33 -0400

    Test fixes: add UMI fastqs, avoid relative paths for Docker Cromwell tests

--- gvcf_joint/run_generate_cwl.sh
+++ gvcf_joint/run_generate_cwl.sh
@@ -6,5 +6,5 @@ PNAME=gvcf-joint
 rm -rf $PNAME-workflow
 bcbio_vm.py cwl --systemconfig=../bcbio_system.yaml gvcf-joint.yaml
 
-BASEDIR=`cd .. && pwd`
-sed -i.bak "s#$BASEDIR/testdata#../../testdata#" $PNAME-workflow/main-$PNAME-samples.json
+#BASEDIR=`cd .. && pwd`
+#sed -i.bak "s#$BASEDIR/testdata#../../testdata#" $PNAME-workflow/main-$PNAME-samples.json
